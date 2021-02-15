20c1718a2a129adfd3b25282c32e19bddf94f4a6
Preshh0 <uniquep201@gmail.com>
2021-02-15 06:41:06 +0100

    New changes.

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+// function display(props){
+//     return props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD"})
+// }
 function Product(props){
     return(
         <div>
             <h2>Name: {props.product.name}</h2>    
+            {/* <button onClick ={ function display(){console.log( props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD"}))}}> Click for product</button> */}
             {/* <button onClick= {props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD"})}> Click for price </button> */}
             <p>
                 {props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD"})} -  {props.product.description}
